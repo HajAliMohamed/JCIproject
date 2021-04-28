358e1694f745ed05900d68f53bcbe3f75e21c708
HajAliMohamed <82762618+HajAliMohamed@users.noreply.github.com>
2021-04-28 19:40:02 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ var menu = document.getElementById('menu');
     icon = document.getElementById('hum');
 let currentSlide = 1;
 
-// Javascript for image slider manual navigation
 var manualNav = function(manual){
   slides.forEach((slide) => {
     slide.classList.remove('active');
@@ -26,6 +25,34 @@ btns.forEach((btn, i) => {
 });
 
 
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
+
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 4000);
+      }
+      repeater();
+    }
+    repeat();
+
 
 menu.style.maxHeight = "0px"
 
@@ -39,6 +66,9 @@ icon.onclick = function()
     else{
         menu.style.maxHeight = "0px" ;
     }
+    
+
+}    ;
     
 
 }    ;
